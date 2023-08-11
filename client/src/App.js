bfa058a6ef36207add660c9f77f7aa5d5cf9fe88
Katsuyuki <katsuyuki.russell@gmail.com>
2023-08-10 21:13:21 -0500

    added bulma (and colored button test)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+
 
 function App() {
   return (
     <div className="App">
+      
+      <div classname="main">
+        <div className="buttons">
+          <button class="button is-primary">Primary</button>
+          <button class="button is-link">Link</button>
+          <button class="button is-info">Info</button>
+          <button class="button is-success">Success</button>
+          <button class="button is-warning">Warning</button>
+          <button class="button is-danger">Danger</button>
+          <button class="button is-black">Black</button>
+          <button class="button is-white">White</button>
+          <button class="button is-dark">Dark</button>
+          <button class="button is-light">Light</button>
+        </div>
+      </div>
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

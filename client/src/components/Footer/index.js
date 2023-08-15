d57e68292dd0f718f2122f034e1d46fa9e29e216
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-08-14 21:10:24 -0500

    Super basic, front end gets users in DB and displays the usernames. Just a test to make sure Front end works with Backend.

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h4>Test Footer</h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

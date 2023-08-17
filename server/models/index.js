56590ec4ad2f7515ef48ea65efa94dd37ea6d625
yusufh18 <yusufhaji01881@gmail.com>
2023-08-16 19:13:33 -0500

    fixing bug

--- server/models/index.js
+++ server/models/index.js
@@ -2,3 +2,35 @@ const User = require('./User');
 const BookList = require('./BookList');
 
 module.exports = { User, BookList };
+
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const userRouter = require("./routes/router");
+
+const app = express();
+
+//database connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+//routes
+app.use(express.json());
+app.use("/api", userRouter);
+
+//port
+app.listen(process.env.PORT, () => {
+  console.log(`port is running ${process.env.PORT}`);
+});
+
+//route -> controllers -> models
